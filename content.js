96babd568dfe4a639b79efe731234bbcbebe80d4
siranet36 <siranet36@gmail.com>
2021-06-27 13:56:34 +0900

    class名の変更に対処

--- content.js
+++ content.js
@@ -5,7 +5,8 @@ function init() {
 		ngNames = names.split(",");
 
 		var result = document.evaluate(
-			"//li[@class='___item___17iVx']",
+			//			"//li[@class='___item___2cHuH']",
+			"//li[contains(@class,'___item___')]",
 			document,
 			null,
 			XPathResult.ORDERED_NODE_ITERATOR_TYPE,
@@ -17,7 +18,36 @@ function init() {
 
 		var element = result.iterateNext();
 		while (element) {
-			//			console.log(element);
+			if (element.childNodes.length < 1) {
+				element = result.iterateNext();
+				continue;
+			} else {
+				if (element.childNodes[0].childNodes.length < 2) {
+					element = result.iterateNext();
+					continue;
+				} else {
+					if (element.childNodes[0].childNodes[1].childNodes.length < 2) {
+						element = result.iterateNext();
+						continue;
+					} else {
+						if (
+							element.childNodes[0].childNodes[1].childNodes[1].childNodes
+								.length < 1
+						) {
+							element = result.iterateNext();
+							continue;
+						} else {
+							if (
+								element.childNodes[0].childNodes[1].childNodes[1].childNodes[0]
+									.childNodes.length < 2
+							) {
+								element = result.iterateNext();
+								continue;
+							}
+						}
+					}
+				}
+			}
 			var atag = element.childNodes[0].childNodes[0];
 			var nametag =
 				element.childNodes[0].childNodes[1].childNodes[1].childNodes[0]
@@ -30,7 +60,8 @@ function init() {
 		}
 
 		result = document.evaluate(
-			"//li[@class='___item___3gSaG']",
+			//			"//li[@class='___item___38SHg']",
+			"//li[contains(@class,'___item___')]",
 			document,
 			null,
 			XPathResult.ORDERED_NODE_ITERATOR_TYPE,
@@ -39,6 +70,40 @@ function init() {
 
 		element = result.iterateNext();
 		while (element) {
+			if (element.childNodes.length < 1) {
+				element = result.iterateNext();
+				continue;
+			} else {
+				if (element.childNodes[0].childNodes.length < 2) {
+					element = result.iterateNext();
+					continue;
+				} else {
+					if (element.childNodes[0].childNodes[0].childNodes.length < 2) {
+						element = result.iterateNext();
+						continue;
+					}
+					if (element.childNodes[0].childNodes[1].childNodes.length < 2) {
+						element = result.iterateNext();
+						continue;
+					} else {
+						if (
+							element.childNodes[0].childNodes[1].childNodes[1].childNodes
+								.length < 1
+						) {
+							element = result.iterateNext();
+							continue;
+						} else {
+							if (
+								element.childNodes[0].childNodes[1].childNodes[1].childNodes[0]
+									.childNodes.length < 2
+							) {
+								element = result.iterateNext();
+								continue;
+							}
+						}
+					}
+				}
+			}
 			var atag = element.childNodes[0].childNodes[0].childNodes[1];
 			var nametag =
 				element.childNodes[0].childNodes[1].childNodes[1].childNodes[0]
